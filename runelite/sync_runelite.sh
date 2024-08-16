e89a2327e375fd4865cf83cd5bc009c6017e6b90
Rahul Nambiar <rahbiar@gmail.com>
2024-08-15 20:18:36 -0400

    add runelite src code sync script

---
+++ runelite/sync_runelite.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TAG="runelite-parent-1.10.36.1"
+REPO_URL="git@github.com:runelite/runelite.git"
+TEMP_DIR="temp-runelite"
+
+# Clone the repository into the temporary directory
+git clone --depth 1 --branch $TAG $REPO_URL $TEMP_DIR
+
+rsync -av --exclude='.git' $TEMP_DIR/ .
+
+rm -rf $TEMP_DIR
+
+echo "Sync complete. Runelite version $TAG has been copied to current directory."
+
