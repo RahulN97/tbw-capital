dcbab4cc1f4f36ee52deefa2416236020b6c2976
Rahul Nambiar <rahbiar@gmail.com>
2024-09-12 19:23:16 -0400

    update rl, add file cleanup to sync script

--- runelite/sync_runelite.sh
+++ runelite/sync_runelite.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-TAG="runelite-parent-1.10.36.1"
+TAG="runelite-parent-1.10.37"
 REPO_URL="git@github.com:runelite/runelite.git"
 TEMP_DIR="temp-runelite"
+
+git -C . clean -ndx -e .idea
+read -p "The above will be deleted and synced back from runelite's git repo. Proceed? (y/n)" confirm
+
+if [ "$confirm" = "y" ]; then
+    echo "Deleting files"
+    git -C . clean -fdx -e .idea
+else
+    echo "Clean operation cancelled"
+    exit 0
+fi
+
+mkdir -p $TEMP_DIR
 
 git clone --depth 1 --branch $TAG $REPO_URL $TEMP_DIR
 
